2243a93bceb13f06ac97043034d23c2ba1e3cc20
yahiakala <24422258+yahiakala@users.noreply.github.com>
2026-01-26 15:22:43 -0500

    Forgot to remove the old code

--- amoni/api.py
+++ amoni/api.py
@@ -253,9 +253,6 @@ def add_submodule(url: str, path: Path, name: str) -> None:
     name
         The name of the submodule
     """
-    repo = pygit2.Repository(".")
-    repo.add_submodule(url, path, callbacks=AmoniRemoteCallbacks())
-    _commit_all(f"Add {name} submodule", repo=repo)
     try:
         subprocess.run(["git", "submodule", "add", url, str(path)], check=True)
         subprocess.run(["git", "add", ".gitmodules", str(path)], check=True)
